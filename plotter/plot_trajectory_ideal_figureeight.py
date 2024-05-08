d797639c913f70fa6501afe6f9ab5c5647dd46e7
albus-fang <albusalbertfang@gmail.com>
2024-05-07 22:45:01 -0400

    plot fixed

--- plotter/plot_trajectory_ideal_figureeight.py
+++ plotter/plot_trajectory_ideal_figureeight.py
@@ -8,7 +8,7 @@ import numpy as np
 import csv
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-
+import datetime
 def draw_circle_3d(radius, height):
     
 
@@ -18,15 +18,27 @@ def draw_circle_3d(radius, height):
     y = radius * np.sin(theta)
     z = np.full_like(theta, height)
     return x,y,z
-def draw_figureeight_3d(radius, height):
+def draw_figureeight_3d(radius, height, length=10000):
     
 
     # Parameters for the circle
-    theta = np.linspace(0, 2*np.pi, 10000)
+    theta = np.linspace(0, 2*np.pi, length)
     y = radius * np.sin(theta)
     x = radius * np.sin(theta) * np.cos(theta)
     z = np.full_like(theta, height)
     return x,y,z
+def generate_coordinates(timestamps, radius = 0.2 , angular=1, height=0.4):
+    timestamps = np.array([timestamps])
+    current_time = timestamps[0]
+    print(timestamps)
+
+    indices = timestamps
+    
+    theta = angular*(indices)
+    x = radius * np.sin(theta) * np.cos(theta)
+    y = radius * np.sin(theta)
+    z = np.full_like(theta, height)
+    return x, y, z
 save = True
 # Open the ROS2 bag file
 #bag = rosbag.Bag('drone_trajectory.bag')
@@ -50,13 +62,22 @@ timestamps = []
 x_ideal = []
 y_ideal = []
 z_ideal = []
+first_second = None
 with Reader(bag_path) as reader:
     for item in reader.connections:
         print(item.topic, item.msgtype)
     for item, timestamp, rawdata in reader.messages():
         if item.topic == topic_name:
             msg = typestore.deserialize_cdr(rawdata, item.msgtype)
-            timestamp = msg.header.stamp.nanosec/10**9
+            
+            # print("nano: ",  msg.header.stamp.nanosec/1e9)
+            if first_second is not None: 
+                print("seconds: ",msg.header.stamp.sec)
+                print("first second: ", first_second)
+                timestamp = msg.header.stamp.sec-first_second + msg.header.stamp.nanosec/10**9
+            else:
+                first_second = msg.header.stamp.sec
+                timestamp = 0
             timestamps.append(timestamp)
             # x_ideal.append(radius * np.cos(angular_vel*(timestamp - timestamps[0])) + center_x)
             # y_ideal.append(radius * np.sin(angular_vel*(timestamp - timestamps[0]))*np.cos(angular_vel*(timestamp - timestamps[0])) + center_y)
@@ -91,12 +112,33 @@ y = np.array(y_data)
 z = np.array(z_data)
 
 x_ideal,y_ideal, z_ideal = draw_figureeight_3d(radius, height)
-
+x_t, y_t, z_t = generate_coordinates(timestamps, radius, angular_vel, height)
 # Scatter plot
 ax.set_zlim(0,0.6)
 ax.scatter(x, y, z, c= 'r')
 ax.scatter(x_ideal, y_ideal, z_ideal, c = 'b')
-plt.savefig("trajectory.png")
+plt.savefig("trajectory_figureeight.png")
+plt.show()
+ 
+
+
+fig = plt.figure()
+ax1 = plt.subplot2grid((3,1), (0,0) , rowspan=1)
+ax1.plot(range(x.size), x, 'r')
+ax1.plot(range(x_t.size), x_t.T, 'b')
+ax1.set_title("x")
+ax1.set_ylabel("meters")
+ax1 = plt.subplot2grid((3,1), (1,0) , rowspan=1)
+ax1.plot(range(y.size), y,'r')
+ax1.plot(range(y_t.size), y_t.T, 'b')
+ax1.set_title("y")
+ax1.set_ylabel("meters")
+ax1 = plt.subplot2grid((3,1), (2,0) , rowspan=1)
+ax1.plot(range(z.size), z,'r')
+ax1.plot(range(z_t.size), z_t.T, 'b')
+ax1.set_ylabel("meters")
+ax1.set_title("z")
+plt.savefig("trajectory_individual_figureeight.png")
 plt.show()
 
 input = input("save trajectory to csv? y/n")
