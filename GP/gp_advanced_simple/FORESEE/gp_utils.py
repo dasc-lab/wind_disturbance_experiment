9deccfa32b417a3361baf740ea9d3990bce48d1a
albus-fang <albusalbertfang@gmail.com>
2024-07-07 21:36:19 -0400

    push

---
+++ GP/gp_advanced_simple/FORESEE/gp_utils.py
@@ -0,0 +1,81 @@
+import jax
+import jax.numpy as jnp
+jax.config.update("jax_enable_x64", True)
+import optax as ox
+import gpjax as jgp
+import pickle
+from gpjax import Dataset
+from jax import jit, random
+key = random.PRNGKey(2)
+home_path = '/Users/albusfang/Coding Projects/gp_ws/Gaussian Process/GP/gp_advanced/FORESEE/'
+
+def get_next_states_with_gp( states, control_inputs, gps, train_x, train_y, dt ):
+
+    test_x = states.T #jnp.append( states.T, control_inputs.T, axis=0)
+
+
+    #################################################
+    ####### Changed: dataset(.reshape) #######
+    # X disturbance
+    #D = Dataset(X=train_x, y=train_y[0])
+    D = Dataset(X=train_x, y=train_y[0].reshape(-1,1))
+    latent_dist = gps[0](test_x, D)
+    predictive_dist = gps[0].likelihood(latent_dist)
+    pred_mean0 = predictive_dist.mean().reshape(-1,1)
+    pred_std0 = predictive_dist.stddev().reshape(-1,1)
+
+    # Y disturbance
+    #D = Dataset(X=train_x, y=train_y[1])
+    D = Dataset(X=train_x, y=train_y[1].reshape(-1,1))
+    latent_dist = gps[1](test_x, D)
+    predictive_dist = gps[1].likelihood(latent_dist)
+    pred_mean1 = predictive_dist.mean().reshape(-1,1)
+    pred_std1 = predictive_dist.stddev().reshape(-1,1)
+
+    # Z disturbance
+    #D = Dataset(X=train_x, y=train_y[2])
+    D = Dataset(X=train_x, y=train_y[2].reshape(-1,1))
+    latent_dist = gps[2](test_x, D)
+    predictive_dist = gps[2].likelihood(latent_dist)
+    pred_mean2 = predictive_dist.mean().reshape(-1,1)
+    pred_std2 = predictive_dist.stddev().reshape(-1,1)
+
+    pred_mu = jnp.concatenate( (pred_mean0.T, pred_mean1.T, pred_mean2.T), axis=0 ) #3x13
+    pred_cov = jnp.concatenate( (pred_std0.T**2, pred_std1.T**2, pred_std2.T**2), axis=0 ) #3x13
+    ################################################
+    ############ bug fix: ########################
+    ############ bug: line 52, incompatible shapes control inputs and pred_mu ############
+    #pred_mu = pred_mu.reshape(3,-1)
+    
+    ################################################
+    next_states_pos = states[0:3] + states[3:6] * dt #+ control_inputs * dt**2/2
+    next_states_vel_mu = states[3:6] + control_inputs * dt + pred_mu * dt
+    next_states_vel_cov = pred_cov * dt * dt
+
+    next_states_mu = jnp.append( next_states_pos, next_states_vel_mu, axis=0 )
+    next_states_cov = jnp.append( jnp.zeros((3,13)), next_states_vel_cov, axis=0 )
+    return next_states_mu, next_states_cov    
+
+def train_gp(likelihood, posterior, parameter_state, train_x, train_y):
+    D = Dataset(X=train_x, y=train_y)
+    negative_mll = jit(posterior.marginal_log_likelihood(D, negative=True))
+    optimiser = ox.adam(learning_rate=0.08)
+    inference_state = jgp.fit(
+        objective=negative_mll,
+        parameter_state=parameter_state,
+        optax_optim=optimiser,
+        num_iters=1500,
+    )
+    learned_params, training_history = inference_state.unpack()
+    return likelihood, posterior, learned_params, D
+
+def initialize_gp_prediction(file_path):#, train_x, train_y): #, test_x):
+    def load_gp_model(file_path):
+        with open(file_path, 'rb') as f:
+            opt_posterior = pickle.load(f)
+        return opt_posterior
+
+    # latent_dist = posterior(gp_params, D)
+    # D = Dataset(X=train_x, y=train_y)
+    opt_posterior = load_gp_model(file_path)
+    return opt_posterior
---
+++ GP/gp_advanced_simple/README.md
@@ -0,0 +1,28 @@
+# Gaussian Data and Model for Wind Experiements
+
+## Reading recorded data, visualizing the trajectories, and truncating takeoff and landing data
+Use `plot_trejectory_ref.py` to visualize the trajectories and its 3D components. Use `bag_path` variable to select different trajectories to be processed. Adjust variables `cutoff` and `threshold` to remove takeoff and landing data, where `threshold` removes takeoff data towards the beginning of the recording and `cutoff` removes landing data from the recording. Please ensure no takeoff and landing data is included as this can mess up Gaussian Process training. Other files such as `plot_acc.py` may import `cutoff` and `threshold`. 
+
+## Visualize Acceleration and Applying Low-Pass Filter
+Use `fft.py` to analyze the signal in frequency domain and visualize the signal pre and post filtering.
+Use `plot_acc.py` to plot out filtered `acc_x`, `acc_y`, and `acc_z`. Use `fft.py` if you want to analyze the accelerations individually.
+**Note**
+`fft_filter(signal, sampling_rate = 5000)` in `plot_acc.py` and `fft.py` calculates the _cutoff frequency_ by adding a hard-coded value to the peak frequency, which may yield varing results. **A more robust way of calculating the cutoff frequency may be desirable**.
+
+## Stitching Dataset and Generate Train and Test Dataset
+
+
+To train GP on all collected datasets with various speeds and coordinates, one would need to crop off the takeoff and landing data for each data set and stitch the cropped datasets together. Run `gp_data_prep.py` to first filter the disturbance and then concatenate the data points together. If the new data set is the same as the existing dataset, the new data won't be concatenated. The concatenated disturbance dataset will be stored in `disturbance.py` and the stitched input dataset will be stored in `input.py`; adjust the name to suit your purpose.
+
+
+# GP Training and Plotting
+
+Use `gp.py` to train gp model for all 3 dimensions of the filtered disturbance with respect to all six inputs. The script saves its training dataset, test dataset, and the whole data set.
+
+## Steps to train GP Model
+1. Use `plot_trajectory_ref.py` to select `bag_path` and adjust `threshold` and `cutoff` to crop off takeoff and landing data.
+2. Optional: use `plot_acc.py` to visualize filtered acc, recorded acc_cmd, and disturbance.
+3. Optional: After running `plot_acc.py`, `recorded_acc.npy` will be saved. Use `fft.py` to read it in and visualize the frequency domain signal.
+4. Use `gp_data_prep.py` to stitch data from various trajectories together to generate the data set for `gp.py`
+5. Run `gp.py` to see the results, which also saves the model into 'gp_models' folder. Adjust the name of the model if necessary.
+6. After `gp.py` has saved the model and images, use load_gp.py to load in the models and see the results for test dataset.
