4f302d223f182fd106a959e2aac819fb878e6659
albus-fang <albusalbertfang@gmail.com>
2024-07-29 00:06:58 -0400

    circle trajectory (0.6,0.0)

--- GP/gp_advanced/plot_trajectory_ref.py
+++ GP/gp_advanced/plot_trajectory_ref.py
@@ -42,10 +42,10 @@ home_path = home_path + 'circle_data/'
 # bag_path = home_path + '28_07_2024_take1_cir_traj_r0.4_w1.0_c0.00_h0.5_kxv19_04_9_30_tank_0_31_fanon_clipped_new'
 #(500, 800)
 
-bag_path = home_path + '28_07_2024_take1_cir_traj_r0.4_w1.0_c0.60_h0.5_kxv7_00_4_00_tank_0_31_fanoff_clipped_new'
+# bag_path = home_path + '28_07_2024_take1_cir_traj_r0.4_w1.0_c0.60_h0.5_kxv7_00_4_00_tank_0_31_fanoff_clipped_new'
 # bag_path = home_path + '28_07_2024_take1_cir_traj_r0.4_w1.0_c0.60_h0.5_kxv7_00_4_00_tank_0_31_fanon_clipped_new'
 # (500,800)
-# bag_path = home_path + '28_07_2024_take1_cir_traj_r0.4_w1.0_c0.60_h0.5_kxv14_81_7_25_tank_0_31_fanon_clipped_new'
+bag_path = home_path + '28_07_2024_take1_cir_traj_r0.4_w1.0_c0.60_h0.5_kxv14_81_7_25_tank_0_31_fanon_clipped_new'
 # bag_path = home_path + '28_07_2024_take1_cir_traj_r0.4_w1.0_c0.60_h0.5_kxv18_86_8_93_tank_0_31_fanon_clipped_new'
 
 
@@ -247,7 +247,7 @@ with Reader(bag_path) as reader:
 # print("z min: ", min(z_data))
 assert len(x_data) == len(y_data) == len(z_data), "Lengths of the lists are not the same."
 cutoff = len(x_data) - 1000
-threshold = 1000
+threshold = 00
 
 
 print("cutoff, threshold = ", cutoff, threshold)
@@ -261,14 +261,14 @@ y = np.array(y_data[threshold:cutoff])
 z = np.array(z_data[threshold:cutoff])
 if save :
     stacked_array = np.column_stack((x,y,z))
-    np.save('compare_trajectory/unoptimized_trajectory_cir_traj_r0.4_w1.0_c0.60_h0.5_kxv7_4_fanoff.npy',stacked_array)
+    np.save('compare_trajectory/optimized_trajectory_cir_traj_r0.4_w1.0_c0.60_h0.5_kxv14_81_7_25.npy',stacked_array)
 #print("size of x data is: ",x.shape)
 x_t = x_ideal = np.array(x_ideal[threshold:cutoff])
 y_t = y_ideal = np.array(y_ideal[threshold:cutoff])
 z_t = z_ideal = np.array(z_ideal[threshold:cutoff])
 if save : 
     stacked_array = np.column_stack((x_t,y_t,z_t))
-    np.save('compare_trajectory/ideal_trajectory_cir_traj_r0.4_w1.0_c0.60_h0.5_fanoff.npy',stacked_array)
+    np.save('compare_trajectory/ideal_trajectory_cir_traj_r0.4_w1.0_c0.60_h0.5_kxv14_81_7_25.npy',stacked_array)
 # Scatter plot
 ax.set_zlim(0,1)
 ax.scatter(x, y, z, c= 'r')
