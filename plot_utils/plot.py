42def9ec7fe51dc136dcffcefa2792e46997de7c
Hardik Parwana <hardikparwana@github.com>
2024-09-03 15:54:59 -0400

    plotting multiple bag files together

--- plot_utils/plot.py
+++ plot_utils/plot.py
@@ -14,6 +14,8 @@ circle_path = recorded_data_path + 'circle_data/'
 figure8_path = recorded_data_path +'figure8_data/'
 
 bag_path = circle_path + '28_07_2024_take1_cir_traj_r0.4_w1.0_c0.00_h0.5_kxv19_04_9_30_tank_0_31_fanon_clipped_new'
+bag_path_unoptimized = circle_path + '28_07_2024_take1_cir_traj_r0.4_w1.0_c0.00_h0.5_kxv7_00_4_00_tank_0_31_fanon_clipped_new'
+bag_path_unoptimized_ideal = circle_path + '28_07_2024_take1_cir_traj_r0.4_w1.0_c0.00_h0.5_kxv7_00_4_00_tank_0_31_fanoff_clipped_new'
 
 # def calculate_reward(pos_ref, pos, vel_ref, vel, pos_factor = 1.0, vel_factor = 0.1):
 #     reward = pos_factor * np.sum(np.fromiter((x**2 for x in (pos-pos_ref)),dtype=float)) + vel_factor * np.sum(np.fromiter((v**2 for v in (vel - vel_ref)),dtype =float))
@@ -65,15 +67,47 @@ def plot_trajectory_reward(bag_path, takeoff, land):
         pos_vector[:,2] = -pos_vector[:,2]
         pos_ref_vector[:,2] = -pos_ref_vector[:,2]
     return pos_vector[takeoff:length-land,:], pos_ref_vector[takeoff:length-land,:], reward_arr[takeoff:length-land], np.sum(reward_arr[takeoff:length-land])
+
+# optimized
 pos_vector, pos_ref_vector, reward_arr, total_reward = plot_trajectory_reward(bag_path,500,900)
 pos_vector = np.array(pos_vector)
 reward_arr = np.array(reward_arr)
-plt.figure()
-plt.plot(reward_arr)
+
+# unoptimized
+pos_vector_unoptimized, pos_ref_vector_unoptimized, reward_arr_unoptimized, total_reward_unoptimized = plot_trajectory_reward(bag_path_unoptimized,500,900)
+pos_vector_unoptimized = np.array(pos_vector_unoptimized)
+reward_arr_unoptimized = np.array(reward_arr_unoptimized) 
+
+# unoptimized ideal
+pos_vector_unoptimized_ideal, pos_ref_vector_unoptimized_ideal, reward_arr_unoptimized_ideal, total_reward_unoptimized_ideal = plot_trajectory_reward(bag_path_unoptimized_ideal,680,700)
+pos_vector_unoptimized_ideal = np.array(pos_vector_unoptimized_ideal)
+reward_arr_unoptimized_ideal = np.array(reward_arr_unoptimized_ideal)
+
+fig1, ax1  =plt.subplots()
+ax1.plot(reward_arr, 'g', label='Optimized')
+ax1.plot(reward_arr_unoptimized, 'k', label='Default')
+# ax1.plot(reward_arr_unoptimized_ideal, 'm', label='Dafault ideal')
+ax1.legend()
+
+fig2 = plt.figure() #(figsize=plt.figaspect(0.5))
+ax2 = fig2.add_subplot(111, projection='3d')
+ns = 10
+width = 2
+ax2.plot(pos_vector[1::ns,0],pos_vector[1::ns,1],pos_vector[1::ns,2], c = 'g', linewidth=width, label='Optimized')
+ax2.plot(pos_ref_vector[1::ns,0],pos_ref_vector[1::ns,1],pos_ref_vector[1::ns,2], c='r',linewidth=width, linestyle='dotted', label='Reference')
+ax2.plot(pos_vector_unoptimized[1::ns,0],pos_vector_unoptimized[1::ns,1],pos_vector_unoptimized[1::ns,2], c = 'k',linewidth=width,  label='Default')
+# ax2.plot(pos_vector_unoptimized_ideal[1::ns,0],pos_vector_unoptimized_ideal[1::ns,1],pos_vector_unoptimized_ideal[1::ns,2], c = 'm',linewidth=width,  label='Default ideal')
+ax2.set_zlim(0,1)
+ax2.legend()
 plt.show()
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.scatter(pos_vector[:,0],pos_vector[:,1],pos_vector[:,2], c = 'r')
-ax.scatter(pos_ref_vector[:,0],pos_ref_vector[:,1],pos_ref_vector[:,2], c = 'b')
-ax.set_zlim(0,1)
-plt.show()
+
+
+# # plt.figure()
+# plt.plot(reward_arr)
+# plt.show()
+# fig = plt.figure()
+# ax = fig.add_subplot(111, projection='3d')
+# ax.scatter(pos_vector[:,0],pos_vector[:,1],pos_vector[:,2], c = 'r')
+# ax.scatter(pos_ref_vector[:,0],pos_ref_vector[:,1],pos_ref_vector[:,2], c = 'b')
+# ax.set_zlim(0,1)
+# plt.show()
