dfd5985a1f47116a274ad93f6b447e892f960b0e
Hardik Parwana <hardikparwana@github.com>
2024-09-07 17:01:33 -0400

    circle and fig8 plots v1 for paper

--- plot_utils/plot.py
+++ plot_utils/plot.py
@@ -8,6 +8,7 @@ import csv
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 recorded_data_path = 'recorded_data/'
+plot_home = "media/"
 
 circle_path = recorded_data_path + 'circle_data/'
 
@@ -16,10 +17,12 @@ figure8_path = recorded_data_path +'figure8_data/'
 bag_path = circle_path + '28_07_2024_take1_cir_traj_r0.4_w1.0_c0.00_h0.5_kxv19_04_9_30_tank_0_31_fanon_clipped_new'
 bag_path_unoptimized = circle_path + '28_07_2024_take1_cir_traj_r0.4_w1.0_c0.00_h0.5_kxv7_00_4_00_tank_0_31_fanon_clipped_new'
 bag_path_unoptimized_ideal = circle_path + '28_07_2024_take1_cir_traj_r0.4_w1.0_c0.00_h0.5_kxv7_00_4_00_tank_0_31_fanoff_clipped_new'
+# 500, 900
 
-bag_path = figure8_path + '28_07_2024_take1_cir_traj_r0.4_w1.0_c0.00_h0.5_kxv19_04_9_30_tank_0_31_fanon_clipped_new'
-bag_path_unoptimized = figure8_path + '28_07_2024_take1_cir_traj_r0.4_w1.0_c0.00_h0.5_kxv7_00_4_00_tank_0_31_fanon_clipped_new'
-bag_path_unoptimized_ideal = figure8_path + '28_07_2024_take1_cir_traj_r0.4_w1.0_c0.00_h0.5_kxv7_00_4_00_tank_0_31_fanoff_clipped_new'
+
+# bag_path = figure8_path + '28_7_2024_eight_traj_r0.4_w1_c080_h0.5_kxv15_16_7_47_fanon_clipped_new'
+# bag_path_unoptimized = figure8_path + '28_7_2024_eight_traj_r0.4_w1_c080_h0.5_kxv7_00_4_00_fanon_clipped_new'
+# bag_path_unoptimized_ideal = figure8_path + '28_7_2024_eight_traj_r0.4_w1_c080_h0.5_kxv7_00_4_00_fanoff_clipped_new'
 
 # def calculate_reward(pos_ref, pos, vel_ref, vel, pos_factor = 1.0, vel_factor = 0.1):
 #     reward = pos_factor * np.sum(np.fromiter((x**2 for x in (pos-pos_ref)),dtype=float)) + vel_factor * np.sum(np.fromiter((v**2 for v in (vel - vel_ref)),dtype =float))
@@ -71,17 +74,20 @@ def plot_trajectory_reward(bag_path, takeoff, land):
     return pos_vector[takeoff:length-land,:], pos_ref_vector[takeoff:length-land,:], reward_arr[takeoff:length-land], np.sum(reward_arr[takeoff:length-land])
 
 # optimized
-pos_vector, pos_ref_vector, reward_arr, total_reward = plot_trajectory_reward(bag_path,500,900)
+pos_vector, pos_ref_vector, reward_arr, total_reward = plot_trajectory_reward(bag_path,500,900) # circle
+# pos_vector, pos_ref_vector, reward_arr, total_reward = plot_trajectory_reward(bag_path,800,1400)
 pos_vector = np.array(pos_vector)
 reward_arr = np.array(reward_arr)
 
 # unoptimized
-pos_vector_unoptimized, pos_ref_vector_unoptimized, reward_arr_unoptimized, total_reward_unoptimized = plot_trajectory_reward(bag_path_unoptimized,500,900)
+pos_vector_unoptimized, pos_ref_vector_unoptimized, reward_arr_unoptimized, total_reward_unoptimized = plot_trajectory_reward(bag_path_unoptimized,500,900) # circle
+# pos_vector_unoptimized, pos_ref_vector_unoptimized, reward_arr_unoptimized, total_reward_unoptimized = plot_trajectory_reward(bag_path_unoptimized,250,1450) 
 pos_vector_unoptimized = np.array(pos_vector_unoptimized)
 reward_arr_unoptimized = np.array(reward_arr_unoptimized) 
 
 # unoptimized ideal
-pos_vector_unoptimized_ideal, pos_ref_vector_unoptimized_ideal, reward_arr_unoptimized_ideal, total_reward_unoptimized_ideal = plot_trajectory_reward(bag_path_unoptimized_ideal,500,1800)
+pos_vector_unoptimized_ideal, pos_ref_vector_unoptimized_ideal, reward_arr_unoptimized_ideal, total_reward_unoptimized_ideal = plot_trajectory_reward(bag_path_unoptimized_ideal,500,1800) # circle
+# pos_vector_unoptimized_ideal, pos_ref_vector_unoptimized_ideal, reward_arr_unoptimized_ideal, total_reward_unoptimized_ideal = plot_trajectory_reward(bag_path_unoptimized_ideal,250,1450)
 pos_vector_unoptimized_ideal = np.array(pos_vector_unoptimized_ideal)
 reward_arr_unoptimized_ideal = np.array(reward_arr_unoptimized_ideal)
 
@@ -89,19 +95,27 @@ fig1, ax1  =plt.subplots()
 ax1.plot(reward_arr, 'g', label='Optimized')
 ax1.plot(reward_arr_unoptimized, 'k', label='Default')
 ax1.plot(reward_arr_unoptimized_ideal, 'm', label='Dafault ideal')
+ax1.set_xlabel('horizon')
+ax1.set_ylabel('Cost')
 ax1.legend()
+fig1.savefig(plot_home+"circle_cost.png")
+fig1.savefig(plot_home+"circle_cost.eps")
 
 fig2 = plt.figure() #(figsize=plt.figaspect(0.5))
 ax2 = fig2.add_subplot(111, projection='3d')
 ns = 10
 width = 2
-ax2.plo
-t(pos_vector[1::ns,0],pos_vector[1::ns,1],pos_vector[1::ns,2], c = 'g', linewidth=width, label='Optimized')
+ax2.plot(pos_vector[1::ns,0],pos_vector[1::ns,1],pos_vector[1::ns,2], c = 'g', linewidth=width, label='Optimized')
 ax2.plot(pos_ref_vector[1::ns,0],pos_ref_vector[1::ns,1],pos_ref_vector[1::ns,2], c='r',linewidth=width, linestyle='dotted', label='Reference')
 ax2.plot(pos_vector_unoptimized[1::ns,0],pos_vector_unoptimized[1::ns,1],pos_vector_unoptimized[1::ns,2], c = 'k',linewidth=width,  label='Default')
 ax2.plot(pos_vector_unoptimized_ideal[1::ns,0],pos_vector_unoptimized_ideal[1::ns,1],pos_vector_unoptimized_ideal[1::ns,2], c = 'm',linewidth=width,  label='Default ideal')
 ax2.set_zlim(0,1)
+ax2.set_xlabel('X (m)')
+ax2.set_ylabel('Y (m)')
+ax2.set_zlabel('Z (m)')
 ax2.legend()
+fig2.savefig(plot_home+"circle_paths.png")
+fig2.savefig(plot_home+"circle_paths.eps")
 plt.show()
 
 
